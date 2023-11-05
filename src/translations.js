3f80d74f2fb377232f04cf909812982400f138fb
AnotherPillow <Thepilous_@outlook.com>
2023-11-05 13:06:19 +0000

    Update translations.js

--- src/translations.js
+++ src/translations.js
@@ -9,6 +9,7 @@ const translations = await new Promise(async (resolve, reject) => {
         if (response1.ok) {
             // If the first URL exists, return its JSON content
             resolve(await response1.json())
+            return;
         }
     } catch (error) {
         // An error occurred while fetching the first URL
@@ -20,6 +21,7 @@ const translations = await new Promise(async (resolve, reject) => {
     
         if (response2.ok) {
             resolve(await response2.json())
+            return;
         }
     } catch (error) {
         alert('Failed to load both local translations and English.')
